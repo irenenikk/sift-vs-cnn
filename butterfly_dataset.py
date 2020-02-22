3086979fd7da8825bfb83c4ff2f39e6ec1d11b51
Irene Nikkarinen <irene.nikkarinen@gmail.com>
2020-02-22 17:54:44 +0000

    Add dataloader for butterflies

---
+++ butterfly_dataset.py
@@ -0,0 +1,45 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+import cv2 as cv
+
+
+class ButterflyDataset(Dataset):
+    """Butterfly 200 dataset."""
+
+    def __init__(self, indices_file, species_file, root_dir, transform=None):
+        """
+        Args:
+            indice_file (string): Path to the csv file split annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.indices = pd.read_csv(indices_file, sep=' ', header=None)
+        self.idx2species = pd.read_csv(species_file, sep=' ', index_col=0, header=None)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.indices)
+
+    def index2species(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+        return [self.idx2species.loc[i].values[0] for i in index]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        label_index = self.indices.iloc[idx, 1]
+        img_name = os.path.join(self.root_dir,
+                                self.indices.iloc[idx, 0])
+        image = cv.imread(img_name)
+        sample = (image, label_index)
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
---
+++ sift_features.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+from butterfly_dataset import ButterflyDataset
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pack_sequence
+from torchvision import transforms
+from skimage import transform
+import torch
+import cv2 as cv
+
+parser = argparse.ArgumentParser(description='Obtain SIFT features for training set')
+parser.add_argument("image_root", type=str,
+                    help="The path to the image data folder")
+parser.add_argument("training_index_file", type=str,
+                    help="The path to the file with training indices")
+parser.add_argument("species_file", type=str,
+                    help="The path to the file with mappings from index to species name")
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample
+        img = transform.resize(image, (self.output_size, self.output_size))
+        return img, label
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    image_root = args.image_root
+    training_index_file = args.training_index_file
+    species_file = args.species_file
+    butterfly_dataset = ButterflyDataset(indices_file=training_index_file,
+                                        root_dir=image_root,
+                                        species_file=species_file,
+                                        transform=transforms.Compose([
+                                               Rescale(256)
+                                        ]))
+    dataloader = DataLoader(butterfly_dataset, batch_size=5, shuffle=True)
+    for x, y in dataloader:
+        species_name = butterfly_dataset.index2species(y)[0]
+        cv.imshow(species_name, x[0].numpy())
+        cv.waitKey()
