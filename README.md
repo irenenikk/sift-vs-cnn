2ef203ff8f59ff3c3ac67e164b57518e8d8c371a
Irene Nikkarinen <irene.nikkarinen@gmail.com>
2020-02-22 18:31:41 +0000

    Add readme and refactor dataloader to own class

---
+++ README.md
@@ -0,0 +1,8 @@
+# Comparisong of SIFT and CNN
+
+This project compares using SIFT with color features and both trained and pretrained CNNs in a butterfly classification task.
+
+## TODO / thoughts
+
+- [ ] Improve data preprocessing when training the neural network by doing random cropping
+- [ ] Make sure the tensor color dimensions are appropriate for OpenCV
---
+++ data_pipeline/dataloader.py
@@ -0,0 +1,34 @@
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from skimage import transform
+from butterfly_dataset import ButterflyDataset
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample
+        img = transform.resize(image, (self.output_size, self.output_size))
+        return img, label
+
+
+def get_butterfly_dataloader(image_root, index_file, species_file, batch_size):
+    butterfly_dataset = ButterflyDataset(indices_file=index_file,
+                                        root_dir=image_root,
+                                        species_file=species_file,
+                                        transform=transforms.Compose([
+                                               Rescale(256)
+                                               # TODO: do these have to be transposed because tensors treat color differently?
+                                        ]))
+    dataloader = DataLoader(butterfly_dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
