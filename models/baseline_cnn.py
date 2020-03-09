6109efcedeaa85a987bb668a346f1ac6e2c0a55f
Irene Nikkarinen <irene.nikkarinen@gmail.com>
2020-03-09 12:08:28 +0000

    Fix bug in grey training

--- models/baseline_cnn.py
+++ models/baseline_cnn.py
@@ -2,19 +2,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BaselineCNN(nn.Module):
-    def __init__(self, maxpool_kernel_size=4, out1=6, kernel1=5, out2=16, kernel2=5):
+    def __init__(self, maxpool_kernel_size=3, out1=8, kernel1=6, out2=20, kernel2=6, grey=False):
         super(BaselineCNN, self).__init__()
         self.out2 = out2
         self.kernel2 = kernel2
-        self.flattened_size = 16*14*14
-        self.conv1 = nn.Conv2d(3, out1, kernel1)
+        in_size = 1 if grey else 3
+        self.flattened_size = 20*26*26
+        self.conv1 = nn.Conv2d(in_size, out1, kernel1)
         self.bn2d = nn.BatchNorm2d(out1)
         self.pool = nn.MaxPool2d(maxpool_kernel_size, maxpool_kernel_size)
         self.conv2 = nn.Conv2d(out1, out2, kernel2)
-        self.fc1 = nn.Linear(self.flattened_size, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 200)
+        self.fc1 = nn.Linear(self.flattened_size, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.fc4 = nn.Linear(512, 200)
         self.drop = nn.Dropout(0.2)
 
     def forward(self, x):
